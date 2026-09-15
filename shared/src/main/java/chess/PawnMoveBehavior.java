b1d188bf6ab04627d8c746d69043873067ba93c7
jpdear <jdearing@byu.edu>
2026-09-14 21:14:35 -0600

    Add classes for knight, pawn, queen, rook, and king movement behaviors

---
+++ shared/src/main/java/chess/PawnMoveBehavior.java
@@ -0,0 +1,10 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class PawnMoveBehavior implements MoveBehavior {
+    @Override
+    public ArrayList<ChessMove> getValidMoves(ChessBoard board, ChessPosition position) {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ shared/src/main/java/chess/QueenMoveBehavior.java
@@ -0,0 +1,10 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class QueenMoveBehavior implements MoveBehavior {
+    @Override
+    public ArrayList<ChessMove> getValidMoves(ChessBoard board, ChessPosition position) {
+        throw new RuntimeException("Not implemented");
+    }
+}
