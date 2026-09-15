7ed1487a12ed4f7e492ca01748ebc254875fde1c
jpdear <jdearing@byu.edu>
2026-09-14 20:10:28 -0600

    Change row and col validation to restrict zero

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -12,11 +12,11 @@ public class ChessPosition {
     private final int col;
 
     public ChessPosition(int row, int col) {
-        if (row > 8 || row < 0) {
+        if (row > 8 || row < 1) {
             throw new IllegalArgumentException("Row must be between 0-8");
         }
 
-        if (col > 8 || col < 0) {
+        if (col > 8 || col < 1) {
             throw new IllegalArgumentException("Col must be between 0-8");
         }
 
