839ab61bdb1ecf802431c4abacbd9d476be5d411
jpdear <jdearing@byu.edu>
2026-09-16 18:45:57 -0600

    Change class to extend MoveBehavior and modify method calls with correct arguments

--- shared/src/main/java/chess/BishopMoveBehavior.java
+++ shared/src/main/java/chess/BishopMoveBehavior.java
@@ -2,43 +2,18 @@ package chess;
 
 import java.util.ArrayList;
 
-public class BishopMoveBehavior implements MoveBehavior {
-    private ArrayList<ChessMove> checkDiagonal(ChessBoard board, ChessPosition position, int rowItr, int colItr) {
-        ArrayList<ChessMove> moves = new ArrayList<>();
-        int firstRow = position.getRow() + rowItr;
-        int firstCol = position.getColumn() + colItr;
-
-        if (!ChessPosition.isValidPosition(firstRow, firstCol)) {
-            return moves;
-        }
-
-        ChessPosition currentCheck = new ChessPosition(firstRow, firstCol);
-
-        while (board.getPiece(currentCheck).getPieceType() == null) {
-            ChessMove validMove = new ChessMove(position, currentCheck);
-            moves.add(validMove);
-
-            int nextRow = currentCheck.getRow() + rowItr;
-            int nextCol = currentCheck.getColumn() + colItr;
-
-            if (!ChessPosition.isValidPosition(nextRow, nextCol)) {
-                break;
-            }
-
-            currentCheck = new ChessPosition(nextRow, nextCol);
-        }
-
-        return moves;
+public class BishopMoveBehavior extends MoveBehavior {
+    public BishopMoveBehavior(ChessBoard board, ChessPosition position) {
+        super(board, position);
     }
 
-    @Override
-    public ArrayList<ChessMove> getValidMoves(ChessBoard board, ChessPosition position) {
+    public ArrayList<ChessMove> getValidMoves() {
         ArrayList<ChessMove> validMoves = new ArrayList<>();
 
-        validMoves.addAll(checkDiagonal(board, position, 1, 1));
-        validMoves.addAll(checkDiagonal(board, position, -1, 1));
-        validMoves.addAll(checkDiagonal(board, position, 1, -1));
-        validMoves.addAll(checkDiagonal(board, position, -1, -1));
+        validMoves.addAll(checkDirection(1, 1, true));
+        validMoves.addAll(checkDirection(-1, 1, true));
+        validMoves.addAll(checkDirection(1, -1, true));
+        validMoves.addAll(checkDirection(-1, -1, true));
 
         return validMoves;
     }
