f246bcf5c49c0db200e24461ab7a8d77bf3700fb
jpdear <jdearing@byu.edu>
2026-09-17 21:56:54 -0600

    Add movement rules for each piece to moveBehaviors map

--- shared/src/main/java/chess/MovementRules.java
+++ shared/src/main/java/chess/MovementRules.java
@@ -13,7 +13,12 @@ public class MovementRules {
 
     public MoveBehavior getMoves(ChessPiece.PieceType pieceType) {
         Map<ChessPiece.PieceType, MoveBehavior> moveBehaviors = Map.of(
-                ChessPiece.PieceType.BISHOP, new BishopMoveBehavior(board, position)
+                ChessPiece.PieceType.BISHOP, new BishopMoveBehavior(board, position),
+                ChessPiece.PieceType.KING, new KingMoveBehavior(board, position),
+                ChessPiece.PieceType.KNIGHT, new KnightMoveBehavior(board, position),
+                ChessPiece.PieceType.QUEEN, new QueenMoveBehavior(board, position),
+                ChessPiece.PieceType.ROOK, new RookMoveBehavior(board, position),
+                ChessPiece.PieceType.PAWN, new PawnMoveBehavior(board, position)
         );
 
         return moveBehaviors.get(pieceType);
