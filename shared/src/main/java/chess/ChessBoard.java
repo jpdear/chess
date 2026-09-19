d252585ad5aac85f9a98756e11c212094d3e8e07
jpdear <jdearing@byu.edu>
2026-09-19 14:00:21 -0600

    Create new function for reseting chess board

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -2,6 +2,8 @@ package chess;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Map;
+import java.util.List;
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -11,10 +13,43 @@ import java.util.Objects;
  */
 public class ChessBoard {
 
-    private ChessPiece[][] board = new ChessPiece[8][8];
+    private final ChessPiece[][] board = new ChessPiece[8][8];
+    private final Map<List<Integer>, ChessPiece> standardLayout = Map.ofEntries(
+        Map.entry(List.of(8, 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK)),
+        Map.entry(List.of(8, 2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT)),
+        Map.entry(List.of(8, 3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP)),
+        Map.entry(List.of(8, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN)),
+        Map.entry(List.of(8, 5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING)),
+        Map.entry(List.of(8, 6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP)),
+        Map.entry(List.of(8, 7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT)),
+        Map.entry(List.of(8, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK)),
+        Map.entry(List.of(7, 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(7, 2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(7, 3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(7, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(7, 5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(7, 6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(7, 7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(7, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(2, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(2, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(2, 3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(2, 4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(2, 5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(2, 6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(2, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(2, 8), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN)),
+        Map.entry(List.of(1, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK)),
+        Map.entry(List.of(1, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT)),
+        Map.entry(List.of(1, 3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP)),
+        Map.entry(List.of(1, 4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN)),
+        Map.entry(List.of(1, 5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING)),
+        Map.entry(List.of(1, 6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP)),
+        Map.entry(List.of(1, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT)),
+        Map.entry(List.of(1, 8), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK))
+    );
 
     public ChessBoard() {
-        
     }
 
     @Override
@@ -25,12 +60,12 @@ public class ChessBoard {
 
         ChessBoard that = (ChessBoard) o;
 
-        return Objects.deepEquals(board, that.board);
+        return Objects.deepEquals(board, that.board) && Objects.equals(standardLayout, that.standardLayout);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Arrays.deepHashCode(board));
+        return Objects.hash(Arrays.deepHashCode(board), standardLayout);
     }
 
     @Override
@@ -75,6 +110,14 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for (ChessPiece[] chessPieces : board) {
+            Arrays.fill(chessPieces, null);
+        }
+
+        for (Map.Entry<List<Integer>, ChessPiece> entry : standardLayout.entrySet()) {
+            List<Integer> coord = entry.getKey();
+            ChessPosition pos = new ChessPosition(coord.get(0), coord.get(1));
+            addPiece(pos, entry.getValue());
+        }
     }
 }
